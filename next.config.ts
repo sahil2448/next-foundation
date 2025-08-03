ce49d2558002c83404093d26c69d60dbf2786700
Sahil Kamble <syk2448@gmail.com>
2025-08-03 15:02:49 +0530

    first commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images:{
+    remotePatterns:[
+      {hostname:"upload.wikimedia.org"}
+    ]
+  }
+};
+
+export default nextConfig;
